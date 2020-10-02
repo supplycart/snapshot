07f11075cce72b6a8391fdf393439f377896987e
Azri Ismail <mazri.iar@gmail.com>
2020-10-02 13:21:10 +0800

    dispatch event after restore

--- src/Events/SnapshotUpdated.php
+++ src/Events/SnapshotUpdated.php
@@ -12,7 +12,7 @@ class SnapshotUpdated
     /**
      * @var \Supplycart\Snapshot\Snapshot
      */
-    public $snapshot;
+    public Snapshot $snapshot;
 
     /**
      * SnapshotCreated constructor.
